6fd28619df114302ea6f477405a50fd48a2a04ab
User7558 <82788197+User7558@users.noreply.github.com>
2022-03-21 20:54:02 -0300

    Add files via upload

---
+++ head/head.ino
@@ -0,0 +1,28 @@
+int led = 10;
+int led2 = 11;
+int led3 = 9;
+
+void setup() 
+{
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
+}
+
+void loop() 
+{
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led3, LOW);
+  delay(1000);
+
+}
