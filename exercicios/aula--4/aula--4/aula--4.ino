80c59475a26f985073789e920db0f7f392d092f0
User7558 <82788197+User7558@users.noreply.github.com>
2023-07-28 19:48:49 -0300

    Create aula--4.ino

---
+++ exercicios/aula--4/aula--4/aula--4.ino
@@ -0,0 +1,37 @@
+#define led 4
+#define botao 3
+int ligou = 0;
+int desligou =0;
+
+void trocaLed(){
+  if (digitalRead(led) == HIGH){
+    digitalWrite(led, LOW);
+  }
+  else{
+    digitalWrite(led, HIGH);
+  }
+}
+
+void keyUp(){
+  if ((ligou == 1) and (desligou == 1)){
+    ligou=0;
+    desligou=0;
+    trocaLed();
+  }
+}
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(botao, INPUT);
+}
+
+void loop() {
+  if (digitalRead(botao) == HIGH){
+    ligou=1;
+    desligou=0;
+  }
+  else{
+    desligou=1;
+  }
+  keyUp();
+}
